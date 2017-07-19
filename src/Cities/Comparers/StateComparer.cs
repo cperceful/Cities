30451dffd08c3f7bf23a73914816d44ef4425c28
cperceful <cpercef1@gmail.com>
2017-07-19 13:56:06 -0500

    Added StateComparer

---
+++ src/Cities/Comparers/StateComparer.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cities.Comparers
+{
+    public class StateComparer : IComparer<City>
+    {
+
+        public int Compare(City x, City y)
+        {
+            return string.Compare(x.State, y.State);
+        }
+
+    }
+}
