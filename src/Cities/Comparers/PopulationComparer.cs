0e26f7fe965847a6b55a623e893e8a8c7380af83
cperceful <cpercef1@gmail.com>
2017-07-19 14:09:06 -0500

    Added PopulationComparer

---
+++ src/Cities/Comparers/PopulationComparer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cities.Comparers
+{
+    public class PopulationComparer : IComparer<City>
+    {
+
+        public int Compare(City x, City y)
+        {
+
+            return y.Population.CompareTo(x.Population);
+        }
+
+    }
+}
